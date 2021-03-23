51a51c12e6de806b1b44310443a02e8ad2c8c42b
Andrzej Jozwiak <andrzej.jozwiak@tomtom.com>
2021-03-24 00:27:12 +0100

    Documentation for the CyclicList

--- src/main/kotlin/org/artificialrevelations/makau/CyclicList.kt
+++ src/main/kotlin/org/artificialrevelations/makau/CyclicList.kt
@@ -3,19 +3,68 @@ package org.artificialrevelations.makau
 import java.lang.IllegalArgumentException
 
 /**
+ * Represents an immutable [Collection] that can be treated as a cycle and
+ * iterated indefinitely. It has a normal [List] at its core but allows getting
+ * elements from it with the use of negative indices or indices exceeding the
+ * size of the base list.
  *
+ * Usage example:
+ *
+ * ```
+ * val list = CyclicList(1, 2, 3)
+ * println(list[0])  // prints: 1
+ * println(list[1])  // prints: 2
+ * println(list[-1]) // prints: 3
+ * println(list[3])  // prints: 1
+ * ```
+ *
+ * It is possible to generate [List]s based on the existing [CyclicList] through
+ * a set of [CyclicList.toList] functions. It is possible to specify the view
+ * as an [IntRange] for the generated [List].
+ *
+ * Usage example:
+ *
+ * ```
+ * val list = CyclicList(1, 2, 3)
+ * println(list.toList())      // prints: [1, 2, 3]
+ * println(list.toList(-2..2)) // prints: [2, 3, 1, 2, 3]
+ * ```
+ *
+ * It is possible to create a new [CyclicList] from the existing one by just
+ * moving the elements by a specified amount. A dedicated [CyclicList.movedBy]
+ * allows to pass an Integer (positive or negative) to receive a new instance
+ * of [CyclicList] that has the elements moved.
+ *
+ * Usage example:
+ *
+ * ```
+ * val list = CyclicList(1, 2, 3)
+ * println(list.movedBy(0))   // prints: CyclicList(1, 2, 3)
+ * println(list.movedBy(1))   // prints: CyclicList(3, 1, 2)
+ * println(list.movedBy(-1))  // prints: CyclicList(2, 3, 1)
+ * println(list.movedBy(3))   // prints: CyclicList(1, 2, 3)
+ * ```
+ *
+ * @see Collection
+ * @see Iterable
+ * @see Iterator
  */
-class CyclicList<A>(private val base: List<A>) : Collection<A> by base, RandomAccess {
-
+class CyclicList<A>(private val base: List<A>) : Collection<A> by base {
     /**
+     * Creates a new instance of [CyclicList] from the specified elements.
      *
+     * @param elements a finite set of elements that will be the base for the
+     * cyclic list.
      */
     constructor(vararg elements: A) : this(listOf(*elements))
 
     // Query Operations
 
     /**
-     *
+     * Returns an iterator over the elements of the cyclic list. A non empty
+     * cyclic list will always indicate the existence of elements. After
+     * reaching the last element of the base list, iterator will start returning
+     * elements from the start.
      */
     override fun iterator(): Iterator<A> =
             object : Iterator<A> {
@@ -23,6 +72,7 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base, RandomAc
                 override fun hasNext() =
                         base.isNotEmpty()
 
+                // TODO: do not increment index indefinitely, it is not needed!
                 override fun next(): A =
                         get(index++)
             }
@@ -39,7 +89,13 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base, RandomAc
                 base[(index % base.size + base.size) % base.size]
 
     /**
+     * Returns a new instance of [CyclicList] that has all the elements moved
+     * by the specified amount (can be negative).
      *
+     * @param number value by which the resulting CyclicList should have its
+     * elements moved.
+     * @return Instance of [CyclicList] with elements moved by the specified
+     * amount.
      */
     infix fun movedBy(number: Int): CyclicList<A> =
             CyclicList(List(base.size) { index ->
@@ -71,11 +127,7 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base, RandomAc
 }
 
 fun main() {
-//    val cyclic5 = CyclicList(1, 2, 3, 4, 5, 6)
-//    println(cyclic5.toList(-1..1))
-    println(CyclicList<Int>().toList(-100..100))
+    var i = 0
+    println(i++ % 3)
+    println(i++ % 3)
 }
-
-fun <A> CyclicList<A>.testMovedBy(number: Int) =
-        println("$this moved by $number is ${movedBy(number)}")
-
