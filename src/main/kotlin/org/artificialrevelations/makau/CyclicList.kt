acef805a3982177b3955b884c11951884c6f6143
Andrzej Jozwiak <andrzej.jozwiak@tomtom.com>
2021-03-24 23:26:00 +0100

    Documentation for the CyclicList and additional query methods

--- src/main/kotlin/org/artificialrevelations/makau/CyclicList.kt
+++ src/main/kotlin/org/artificialrevelations/makau/CyclicList.kt
@@ -65,6 +65,8 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base {
      * cyclic list will always indicate the existence of elements. After
      * reaching the last element of the base list, iterator will start returning
      * elements from the start.
+     *
+     * @return Iterator that allows to go over the elements of the cyclic list.
      */
     override fun iterator(): Iterator<A> =
             object : Iterator<A> {
@@ -80,13 +82,45 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base {
     // Positional Access Operations
 
     /**
+     * Returns element at the specified index of the [CyclicList]. The index
+     * can be negative or can exceed the size of cyclic list.
      *
+     * @param index Position within the cyclic list for which the element is
+     * supposed to be returned.
+     * @return Element that is stored under the specified index.
+     * @throws IllegalArgumentException If the [CyclicList] is empty.
      */
     operator fun get(index: Int): A =
-            if (base.isEmpty())
-                throw IllegalArgumentException("CyclicList is empty!")
+            getOrNull(index)
+                    ?: throw IllegalArgumentException("CyclicList is empty!")
+
+    /**
+     * Returns element at the specified index of the [CyclicList]. The index
+     * can be negative or can exceed the size of cyclic list.
+     *
+     * @param index Position within the cyclic list for which the element is
+     * supposed to be returned.
+     * @return Element that is stored under the specified index or null if the
+     * cyclic list is empty.
+     */
+    fun getOrNull(index: Int): A? =
+            if (isEmpty())
+                null
             else
                 base[(index % base.size + base.size) % base.size]
+
+    /**
+     * Returns element at the specified index of the [CyclicList] or runs the
+     * specified function if the list is empty. The index can be negative or
+     * can exceed the size of cyclic list.
+     *
+     * @param index Position within the cyclic list for which the element is
+     * supposed to be returned.
+     * @return Element that is stored under the specified index or value
+     * returned by the specified function.
+     */
+    inline fun getOrElse(index: Int, emptyValue: () -> A): A =
+            if (isEmpty()) emptyValue() else get(index)
 
     /**
      * Returns a new instance of [CyclicList] that has all the elements moved
@@ -103,31 +137,37 @@ class CyclicList<A>(private val base: List<A>) : Collection<A> by base {
             })
 
     /**
+     * Returns the base [List] that is behind this [CyclicList]. This method is
+     * useful when wanting to iterate through the contents of the [CyclicList]
+     * with usual means like for loops or [List.forEach] calls without expose
+     * oneself to potential infinite loops.
      *
+     * @return Underlying base list.
      */
     fun toList(): List<A> =
             base
 
     /**
+     * Returns a [List] constructed from the values stored under indices
+     * represented as the specified range. Resulting [List] can be larger or
+     * smaller then the base underlying list.
      *
+     * @return List created from the elements of the cyclic list.
      */
     fun toList(range: IntRange): List<A> =
-            if (base.isEmpty())
-                emptyList()
-            else
-                range.map { index -> get(index) }
+            if (isEmpty()) emptyList() else range.map { index -> get(index) }
+
 
     /**
+     * Returns a string representation of the [CyclicList]. This string will
+     * contain comma separated string representations of contained elements.
+     * If this Cyclic list contains references to itself then for each such
+     * element a "(this Collection)" will be printed.
      *
+     * @return string representation of the [CyclicList]
      */
     override fun toString(): String =
             base.joinToString(", ", "CyclicList(", ")") {
                 if (it === this) "(this Collection)" else it.toString()
             }
-}
-
-fun main() {
-    var i = 0
-    println(i++ % 3)
-    println(i++ % 3)
 }
