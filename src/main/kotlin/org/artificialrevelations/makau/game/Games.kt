b4c8d89ef15629bd1a8d41614378111fa1e7a116
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2021-04-15 23:42:10 +0200

    Cleanup

--- src/main/kotlin/org/artificialrevelations/makau/game/Games.kt
+++ src/main/kotlin/org/artificialrevelations/makau/game/Games.kt
@@ -1,6 +1,25 @@
 package org.artificialrevelations.makau.game
 
 /*
+Topics:
+
+- ForfeitGame capability sent always for every player? or sent only for the
+active player? Should non active players need to wait for their turn to forfeit
+the game?
+
+- How to store information about the current played card state and previously
+played cards? This has the most use for 2, 3, 4s because of cumulative effect
+on the next player. This has use for Jack which demands a specific card. In case
+of playing Jack and demanding a card, the active player who demanded the card
+is supposed to play that card last. Each player needs to play the demanded card,
+draw a card or play a Jack and demand a different card.
+
+- Going from Game state to Game state through capabilities
+getCapabilities :: Game -> [Capability]
+run  :: Capability -> Game
+ */
+
+/*
 Rules:
 1) 2s force next player to draw 2 cards or place another 2s
 2) 3s force next player to draw 3 cards or place another 3s
